f7ce17fb51c629585a3fc64d6721600ed97ff551
Reneghis1999 <viceprojects19@gmail.com>
2026-03-23 09:51:36 +0000

    004-add-customer-and-invoicepage

--- fact_app/enums/invoice_type.py
+++ fact_app/enums/invoice_type.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class InvoiceType(models.TextChoices):
-    RECEIPT = "R", "RECU"
-    PROFORMA = "P", "PROFORMA FACTURE"
-    FACTURE = "F", "FACTURE"
+    RECEIPT = "R", "RECIEPT"
+    PROFORMA = "P", "PROFORMA INVOICE"
+    FACTURE = "I", "INVOICE"
--- fact_app/models/customer.py
+++ fact_app/models/customer.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from fact_app.models.base_model import BaseModel 
 
+
 class Customer(BaseModel):
 
     SEX_TYPES = (
@@ -9,15 +10,22 @@ class Customer(BaseModel):
         ('F', 'Feminin'),
     )
 
+    AGE_CHOICES = (
+        ('0-15', '0-15'),
+        ('15-25', '15-25'),
+        ('25-40', '25-40'),
+        ('40+', '40+'),
+    )
+
     name = models.CharField(max_length=132)
     email = models.EmailField()
     phone = models.CharField(max_length=132)
     address = models.CharField(max_length=64)
     sex = models.CharField(max_length=1, choices=SEX_TYPES)
-    age = models.IntegerField()  # <-- changer en IntegerField
+    age = models.CharField(max_length=10, choices=AGE_CHOICES)  # corrigé
     city = models.CharField(max_length=32)
     zip_code = models.CharField(max_length=16)
-    save_by = models.ForeignKey(User, on_delete=models.PROTECT)  # utilisateur qui a créé le client
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
 
     class Meta:
         verbose_name = "Customer"
